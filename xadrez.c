a8143fc394c1dda867cf1ed262b36530d175f216
jnrxxw <ultraff2014@gmail.com>
2025-05-18 01:11:57 +0000

    Desafio: nível novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,68 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+// variaveis
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+int torre = 1;
+int rainha;
+int bispo = 1;
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+// movimentos da torre
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+printf ("***Movimentos da torre***\n");
 
+while (torre <= 5) {
+
+  printf (" Moveu-se para direita\n") ;
+
+torre++;
+
+}
+
+
+
+// movimentos do bispo
+ 
+
+ printf ("\n***Movimentos do bispo***\n");
+
+do { 
+    
+    
+
+    printf (" Moveu-se para cima, direita\n") ;
+    bispo++;
+
+
+} while (bispo<= 5) ;
+
+  
+
+// movimentos da rainha
+
+
+
+
+printf ("\n***Movimentos da rainha***\n");
+
+for (rainha = 1 ; rainha <= 8 ; rainha++) {
+
+
+ printf ("Moveu-se para esquerda\n");
+
+
+}
+
+
+
+
+
+    
     return 0;
 }
